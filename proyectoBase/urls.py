5634882a7d0f46b5e11de3796f53d558ae814e40
Alumnos Copiapó Laboratorio 406L <COP406L@inacap.cl>
2026-09-01 16:13:02 -0400

    segundo commit

--- proyectoBase/urls.py
+++ proyectoBase/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nexoDigital/', include('NexoDigitalSoluciones'))
+    path('', include('NexoDigitalSoluciones'))
 ]
